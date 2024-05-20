d1a80e865efca8b78717f9db7e990f34f7a4b2da
Shansing <i@shansing.com>
2024-05-21 01:47:21 +0800

    fix: build

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -140,9 +140,9 @@ function Screen() {
     <div
       className={
         styles.container +
-        ` ${shouldTightBorder ? styles["tight-container"] : styles.container} ${
-          getLang() === "ar" ? styles["rtl-screen"] : ""
-        }`
+        ` ${
+          shouldTightBorder ? styles["tight-container"] : styles.container
+        } ${""}`
       }
     >
       {isAuth ? (
