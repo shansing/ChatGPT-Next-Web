6dde19a694e1684e0eac2d258709847c82153c5d
Shansing <i@shansing.com>
2024-11-06 20:55:26 +0800

    feat: Claude 3.5 Haiku

--- app/constant.ts
+++ app/constant.ts
@@ -130,7 +130,7 @@ Latex block: $$e=mc^2$$
 export const GPT_4_MODEL = "gpt-4o-2024-08-06";
 // export const GPT_35_MODEL = "gpt-3.5-turbo-0125";
 export const GPT_4_MINI_MODEL = "gpt-4o-mini-2024-07-18";
-export const CLAUDE_HAIKU = "claude-3-haiku-20240307";
+export const CLAUDE_HAIKU = "claude-3-5-haiku-latest";
 export const CLAUDE_SONNET = "claude-3-5-sonnet-latest";
 export const QWEN_LONG = "qwen-long";
 
@@ -165,6 +165,8 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   "claude-3-opus-20240229": "2023-08",
   "claude-3-sonnet-20240229": "2023-08",
   "claude-3-haiku-20240307": "2023-08",
+  "claude-3-5-haiku-20241022": "2024-07",
+  "claude-3-5-haiku-latest": "2024-07",
   "claude-3-5-sonnet-20240620": "2024-04",
   "claude-3-5-sonnet-20241022": "2024-04",
   "claude-3-5-sonnet-latest": "2024-04",
@@ -209,6 +211,8 @@ const anthropicModels = [
   "claude-3-5-sonnet-latest",
   "claude-3-5-sonnet-20241022",
   "claude-3-5-sonnet-20240620",
+  "claude-3-5-haiku-latest",
+  "claude-3-5-haiku-20241022",
   "claude-3-opus-20240229",
   "claude-3-sonnet-20240229",
   "claude-3-haiku-20240307",
