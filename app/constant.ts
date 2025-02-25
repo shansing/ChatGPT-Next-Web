4d8b8ba5cdc41066182e7ffbe7bad993e927f8ea
Shansing <i@shansing.com>
2025-02-25 20:08:08 +0800

    feat: anthropic/claude-3.7-sonnet

--- app/constant.ts
+++ app/constant.ts
@@ -137,7 +137,7 @@ export const GPT_4_MODEL = "gpt-4o-2024-11-20";
 // export const GPT_35_MODEL = "gpt-3.5-turbo-0125";
 export const GPT_4_MINI_MODEL = "gpt-4o-mini-2024-07-18";
 //temporarily disable claude
-export const CLAUDE_SONNET = "anthropic/claude-3.5-sonnet"; //claude-3-5-sonnet-latest
+export const CLAUDE_SONNET = "anthropic/claude-3.7-sonnet"; //claude-3-5-sonnet-latest
 export const CLAUDE_HAIKU = CLAUDE_SONNET; //claude-3-5-haiku-latest
 export const QWEN_LONG = "qwen-long";
 
@@ -187,6 +187,7 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   "claude-3-5-sonnet-20241022": "2024-04",
   "claude-3-5-sonnet-latest": "2024-04",
   "anthropic/claude-3.5-sonnet": "2024-04",
+  "anthropic/claude-3.7-sonnet": "2024-10",
 };
 
 const openaiModels = [
@@ -276,6 +277,7 @@ const alibabaModels = [
 const openRouterModels = [
   "deepseek/deepseek-chat",
   "deepseek/deepseek-r1",
+  "anthropic/claude-3.7-sonnet",
   "anthropic/claude-3.5-sonnet",
   "openai/o3-mini-high",
   "openai/o3-mini",
@@ -365,6 +367,12 @@ export const modelThresholdTokenNumbers = [
   { name: "qwen-long", total: null, prompt: 9_000, completion: 2000 }, // total is not 10_000_000
   { name: "gemini-2.0-", total: null, prompt: 1_048_576, completion: 8192 },
   { name: "gemini-", total: null, prompt: 128_000, completion: 8192 }, //1.5flash 1,048,576;  1.5pro 2,097,152;  but under 128k is cheap
+  {
+    name: "anthropic/claude-3.7",
+    total: 200_000,
+    prompt: null,
+    completion: 64_000,
+  },
   {
     name: "anthropic/claude-3.5-sonnet",
     total: 200_000,
