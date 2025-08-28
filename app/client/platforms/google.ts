5ba851c61b32acc4ee38426904146280afe278c3
Shansing <i@shansing.com>
2025-08-28 19:10:56 +0800

    style: gemini url_context tool

--- app/client/platforms/google.ts
+++ app/client/platforms/google.ts
@@ -147,6 +147,7 @@ export class GeminiProApi implements LLMApi {
               //   }
               // },
               googleSearch: {},
+              urlContext: {},
             },
           ]
         : []),
