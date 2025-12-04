e3f06ad35123adbb13d83ff9ee20b01ebcd54fdf
Shansing <i@shansing.com>
2025-12-04 23:04:31 +0800

    fix: persisted available models ard not be update after source code have been updated
    https://github.com/ChatGPTNextWeb/NextChat/commit/9e1e0a72521cc84ef74499195f3734850b9ccd13

--- app/store/config.ts
+++ app/store/config.ts
@@ -147,6 +147,21 @@ export const useAppConfig = createPersistStore(
   {
     name: StoreKey.Config,
     version: 3.9,
+
+    merge(persistedState, currentState) {
+      const state = persistedState as ChatConfig | undefined;
+      if (!state) return { ...currentState };
+      const models = currentState.models.slice();
+      state.models.forEach((pModel) => {
+        const idx = models.findIndex(
+          (v) => v.name === pModel.name && v.provider === pModel.provider,
+        );
+        if (idx !== -1) models[idx] = pModel;
+        else models.push(pModel);
+      });
+      return { ...currentState, ...state, models: models };
+    },
+
     migrate(persistedState, version) {
       const state = persistedState as ChatConfig;
 
